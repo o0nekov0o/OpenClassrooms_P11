cebc176ac407d4f04845afc72e45068d23441d22
o0nekov0o <nydeanor@gmail.com>
2024-02-26 02:17:48 +0100

    read only table for clubs at homepage

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ clubs = loadClubs()
 
 @app.route('/')
 def index():
-    return render_template('index.html', competitions=competitions)
+    return render_template('index.html', clubs=clubs)
 
 
 @app.route('/showSummary', methods=['POST'])
