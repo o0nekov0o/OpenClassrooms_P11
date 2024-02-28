506b1bcff1b6f09e11d4e4f4980fc2f0a53ecb0a
o0nekov0o <nydeanor@gmail.com>
2024-02-28 22:38:50 +0100

    modifications noms dossiers (intégration) ajout de tous les tests unitaires aussi modifications noms branches (qa et enhancement) modifier docstring dans les tests unitaires mettre rapport coverage dans livrables modifier json date et points clubs

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ clubs = loadClubs()
 
 @app.route('/')
 def index():
-    return render_template('index.html', club=clubs)
+    return render_template('index.html', clubs=clubs)
 
 
 @app.route('/showSummary', methods=['POST'])
